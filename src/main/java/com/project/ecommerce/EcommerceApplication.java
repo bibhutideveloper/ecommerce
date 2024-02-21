85ca3fc4014a3b1d011b261ade16ca098ac14859
Bibhuti Kumbhakar <bibhutikumbhakar@gmail.com>
2024-02-21 15:55:06 +0530

    update

--- src/main/java/com/project/ecommerce/EcommerceApplication.java
+++ src/main/java/com/project/ecommerce/EcommerceApplication.java
@@ -2,12 +2,17 @@ package com.project.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
 }
+
+
+
+
